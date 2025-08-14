6a8dc40dc80238e72ab29f3bff8f85448c9dbdff
Serg-Ale <sergioalexandre0716@gmail.com>
2025-08-14 11:54:01 -0300

    feat(tests): enhance Header component tests with navigation functionality

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,33 @@
-import {render, screen} from '@testing-library/react'
-import Header from './Header'
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router';
+import Header from './Header';
 
+const mockNavigate = jest.fn();
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useNavigate: () => mockNavigate,
+}));
+
+const renderHeader = () => {
+  render(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  );
+};
 
 describe('Header', () => {
   test('renders Header component', () => {
-    render(<Header />);
+    renderHeader();
     expect(screen.getByTestId('header')).toBeInTheDocument();
-  })
+  });
+  test('should navigate to home on logo click', async () => {
+    renderHeader();
+    const user = userEvent.setup();
+    const logo = screen.getByAltText('logo');
+    await user.click(logo);
+    expect(mockNavigate).toHaveBeenCalledWith('/');
+    expect(mockNavigate).not.toHaveBeenCalledWith('/cart');
+  });
 });
