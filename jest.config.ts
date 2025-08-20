1b513025c46e27a1a7efe42197d043b4b983f8cd
Serg-Ale <sergioalexandre0716@gmail.com>
2025-08-20 09:57:29 -0300

    perf(test): optimize Jest configuration for better performance

--- jest.config.ts
+++ jest.config.ts
@@ -4,12 +4,24 @@ export default {
   testEnvironment: 'jsdom',
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.test.json' }]
+    '^.+\\.(ts|tsx)$': ['ts-jest', { 
+      useESM: true, 
+      tsconfig: 'tsconfig.test.json'
+    }]
   },
   moduleNameMapper: {
     '^.+\\.(css|scss|sass|less)$': 'identity-obj-proxy',
   '^.+\\.(svg|png|jpg|jpeg|gif|bmp|webp)$': '<rootDir>/src/test-utils/fileMock.ts',
   '^@/(.*)$': '<rootDir>/src/$1'
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  // Performance optimizations
+  maxWorkers: '50%',
+  testTimeout: 10000,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/test-utils/**',
+    '!src/**/__tests__/**'
+  ]
 }
